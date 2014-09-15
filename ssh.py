922d6f2e22b9c57a978e21bbb5ca279dbb5b34f7
guojian <g975291783@gmail.com>
2014-09-15 20:34:31 +0800

    添加ssh.py文件，并在README.md中加入了使用方法

---
+++ ssh.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import commands
+import os
+import sys
+
+def start():
+    os.system("ssh -qfnNT -D 127.0.0.1:7070 fastssh.com-guojian@usa-public2.fastssh.com")
+    #os.system('ssh -qfnNT -D 127.0.0.1:7070 fastssh.com-guojian-ph@us-phoenix.fastssh.com')
+    #os.system('ssh -qfnNT -D 127.0.0.1:7070 fastssh.com-guojian-jp@jp-1.fastssh.com')
+
+def kill():
+    status,output = commands.getstatusoutput('ps aux|grep "ssh -qfnNT"')
+    result = output.split("\n")
+    for i in range(len(result)-2):
+        id = result[i].split("  ")[1]
+        commands.getstatusoutput("kill "+id)
+        print(id+" have been killed")
+
+def list():
+    status,output = commands.getstatusoutput('ps aux|grep "ssh -qfnNT"')
+    result = output.split("\n")
+    for i in range(len(result)-2):
+        print(result[i])
+
+def main():
+    choose = sys.argv.pop()
+
+    if choose == 'start':
+        start()
+    elif choose == 'list':
+        list()
+    elif choose == 'kill':
+        kill()
+    else:
+        print("error opreator")
+
+if __name__ == '__main__':
+    main()
